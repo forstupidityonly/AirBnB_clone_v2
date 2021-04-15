2be1d9eb730cf441c7160617806db8b6d1705af4
brerickner <breannarickner@gmail.com>
2021-04-15 13:42:02 +0000

    might help to push the right version

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,10 +3,11 @@
 from datetime import datetime
 from fabric.api import local
 
+
 def do_pack():
     '''Method to generate .tgz archive'''
     local("sudo mkdir -p versions")
     timeStr = datetime.now().strftime('%Y%m%d%H%M%S')
     tgzName = "versions/web_static_{}.tgz".format(timeStr)
-    local("sudo tar --create -v --file={} web_static".format(tgzName))
+    local("sudo tar -cvzf {} web_static".format(tgzName))
     return (tgzName)
