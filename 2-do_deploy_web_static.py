57d236dc23e571c3e55f80970ed9f8246a002774
brerickner <breannarickner@gmail.com>
2021-04-15 04:34:58 +0000

    yayyyyy! have hbnb static pages on both servers. pepfixed. output verified

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,23 +9,25 @@ import os
 env.user = 'ubuntu'
 env.hosts = ['34.75.36.193', '3.94.194.152']
 
+
 def do_deploy(archive_path):
     '''Method to deploy archive'''
 
+    arch = archive_path.split("/", 2)[1]
     newFile = "/data/web_static/releases/{}".format(
-        archive_path.split(".", 1)[0])
-    tmpFile = "/tmp/{}".format(archive_path)
+        arch.split(".", 1)[0])
+    tmpFile = "/tmp/{}".format(arch)
     cur = "/data/web_static/current"
 
     if os.path.exists(archive_path) is False:
         return False
 
     try:
-        put("{}, '/tmp/'".format(archive_path))
-        sudo("mkdir -p {}".format(newFile))
+        put(archive_path, '/tmp/')
+        sudo("mkdir -p {}/".format(newFile))
         sudo("tar -xzf {} -C {}/".format(tmpFile, newFile))
         sudo("rm {}".format(tmpFile))
-        sudo("mv {}/web_static/* {}/".format(newFile, newFile))
+        sudo("mv {}/web_static/* {}".format(newFile, newFile))
         sudo("rm -rf {}/web_static".format(newFile))
         sudo("rm -rf {}".format(cur))
         sudo("ln -s {} {}".format(newFile, cur))
