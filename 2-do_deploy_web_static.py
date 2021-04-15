4f38555b021f7e64c06112743ff2568d712670b8
brerickner <breannarickner@gmail.com>
2021-04-15 16:32:10 +0000

    okay this is seriously working perfectly. pepfixed. everything

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,31 +16,30 @@ def do_deploy(archive_path):
 
     if os.path.exists(archive_path) is False:
         return False
-    
+
     arch = archive_path.split("/", 2)[1]
-    new_file = "/data/web_static/releases/{}".format(
+    new_file = '/data/web_static/releases/{}'.format(
         arch.split(".", 1)[0])
-    tmp_file = "/tmp/{}".format(arch)
-    cur = "/data/web_static/current"
+    tmp_file = '/tmp/{}'.format(arch)
+    cur = '/data/web_static/current'
 
     '''
-    archive_path = versions/web_static_20210415041353.tgz
-    arch = web_static_20210415041353.tgz
-    new_file = /data/web_static/releases/web_static_20210415041353
-    tmp_file = /tmp/web_static_20210415041353.tgz
+    archive_path = versions/web_static_20210415160740.tgz
+    arch = web_static_20210415160740.tgz
+    new_file = /data/web_static/releases/web_static_20210415160740
+    tmp_file = /tmp/web_static_20210415160740.tgz
     cur = /data/web_static/current
     '''
 
     try:
         put(archive_path, '/tmp/')
-        run("mkdir -p {}/".format(new_file))
-        print(made it passed mkdir!!)
-        run("tar -xzf {} -C {}/".format(tmp_file, new_file))
-        run("rm {}".format(tmp_file))
-        run("mv {}/web_static/ {}".format(new_file, new_file))
-        run("rm -rf {}/web_static".format(new_file))
-        run("rm -rf {}".format(cur))
-        run("ln -s {} {}".format(new_file, cur))
+        sudo("mkdir -p {}/".format(new_file))
+        sudo("tar -xzf {} -C {}/".format(tmp_file, new_file))
+        sudo("rm {}".format(tmp_file))
+        sudo("mv {}/web_static/* {}".format(new_file, new_file))
+        sudo("rm -rf {}/web_static".format(new_file))
+        sudo("rm -rf {}".format(cur))
+        sudo("ln -s {}/ {}".format(new_file, cur))
         print("YAY! LOVE YOU!")
         return(True)
     except:
