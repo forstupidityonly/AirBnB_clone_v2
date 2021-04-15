30d50f4eeafd4b8d5adb1db6d5d7f334a240f9cf
brerickner <breannarickner@gmail.com>
2021-04-15 15:53:30 +0000

    adding key for what variables are equal to for debugging purposes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Module distributes an archive to your web servers
 using the function do_deploy'''
+
 from datetime import datetime
 from fabric.api import local, env, put, sudo
 import os
@@ -13,27 +14,35 @@ env.hosts = ['34.75.36.193', '3.94.194.152']
 def do_deploy(archive_path):
     '''Method to deploy archive'''
 
-    arch = archive_path.split("/", 2)[1]
-    newFile = "/data/web_static/releases/{}".format(
-        arch.split(".", 1)[0])
-    tmpFile = "/tmp/{}".format(arch)
-    cur = "/data/web_static/current"
-
     if os.path.exists(archive_path) is False:
         return False
+    
+    arch = archive_path.split("/", 2)[1]
+    new_file = "/data/web_static/releases/{}".format(
+        arch.split(".", 1)[0])
+    tmp_file = "/tmp/{}".format(arch)
+    cur = "/data/web_static/current"
+
+    '''
+    archive_path = versions/web_static_20210415041353.tgz
+    arch = web_static_20210415041353.tgz
+    new_file = /data/web_static/releases/web_static_20210415041353
+    tmp_file = /tmp/web_static_20210415041353.tgz
+    cur = /data/web_static/current
+    '''
 
     try:
         put(archive_path, '/tmp/')
-        print("put worked")
-        run("mkdir -p {}/".format(newFile))
-        print("mkdir worked")
-        run("tar -xzf {} -C {}/".format(tmpFile, newFile))
-        print("tar worked")
-        run("rm {}".format(tmpFile))
-        run("mv {}/web_static/ {}".format(newFile, newFile))
-        run("rm -rf {}/web_static".format(newFile))
+        run("mkdir -p {}/".format(new_file))
+        print(made it passed mkdir!!)
+        run("tar -xzf {} -C {}/".format(tmp_file, new_file))
+        run("rm {}".format(tmp_file))
+        run("mv {}/web_static/ {}".format(new_file, new_file))
+        run("rm -rf {}/web_static".format(new_file))
         run("rm -rf {}".format(cur))
-        run("ln -s {} {}".format(newFile, cur))
+        run("ln -s {} {}".format(new_file, cur))
+        print("YAY! LOVE YOU!")
         return(True)
     except:
+        print("KEEP TRYING! :)")
         return(False)
