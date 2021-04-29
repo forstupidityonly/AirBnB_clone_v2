1dbcc6234563fb2a06d2f7613c8def472554d556
brerickner <breannarickner@gmail.com>
2021-04-29 15:07:40 +0000

    adding 0. pep8 good. output good

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Module to start Flask web app'''
+from flask import Flask
+
+app = Flask(__name__)
+HOST = '0.0.0.0'
+PORT = 5000
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    '''method that returns hello world'''
+    return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run(HOST, PORT)
