919da04c3987578bb46f94acb78241fd0a9a33f7
Corbin <forstupidityonly@gmail.com>
2021-05-03 17:50:08 -0500

    why wont it work

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,25 +2,23 @@
 """flask web app to fun ab&b"""
 
 from flask import Flask, render_template
-app = Flask(__name__)
-app.url_map.strict_slashes = False
 from models import storage
 from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
 
 @app.route('/states_list')
 def states_list():
     """states list"""
-    states_list = storage.all("State").values()
-    for i in state_list:
-        if isinstance(i, State):
-            i = state.to_dict()
-        return render_template("7-states_list.html", states=state_list)
+    states_list = storage.all(State)
+    return render_template('7-states_list.html', state=states_list)
 
 @app.teardown_appcontext
-def tear_down():
+def tear_down(context):
     """close"""
     storage.close()
-
 
 if __name__ == '__main__':
     app.run("0.0.0.0", 5000)
