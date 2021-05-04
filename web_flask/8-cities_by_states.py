1a180117a8985f9fdd5b2a90d8329ee525788040
Corbin <forstupidityonly@gmail.com>
2021-05-03 20:34:13 -0500

    im so very over this project

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""flask web app to fun ab&b"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/cities_by_states')
+def states_list():
+    """states list"""
+    states_list = storage.all(State)
+    return render_template('7-states_list.html', states_list=states_list)
+
+
+@app.teardown_appcontext
+def tear_down(context):
+    """close"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
