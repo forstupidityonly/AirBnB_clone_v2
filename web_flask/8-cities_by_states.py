ad6acca6a4c6bdb65a87af4ca78a598074cd86b9
Corbin <forstupidityonly@gmail.com>
2021-05-03 20:40:12 -0500

    correct html file

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -13,7 +13,7 @@ app.url_map.strict_slashes = False
 def states_list():
     """states list"""
     states_list = storage.all(State)
-    return render_template('7-states_list.html', states_list=states_list)
+    return render_template('8-cities_by_states.html', states_list=states_list)
 
 
 @app.teardown_appcontext
